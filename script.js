574acd2b430825061ffd50c3c6ea0643c2d1b40f
shatee-ak <abithe99@gmail.com>
2025-01-31 20:36:31 +0600

    let

---
+++ script.js
@@ -0,0 +1,13 @@
+let total = 500;
+let egg = 5;
+let eachegg=15;
+let kitkat =2
+let eachkitkat = 40;
+
+let eggprice = eachegg*egg;
+console .log(eggprice);
+let kitkatprice = eachkitkat*kitkat;
+console .log(kitkatprice);
+let totalprice = eggprice+kitkatprice;
+let remainingmoney = total-totalprice;
+console.log(remainingmoney);
